e0749bcc43dc5f776c2264394de8b8f0a8a4deff
hrid1 <hridoycse1@gmail.com>
2024-09-27 00:00:08 +0600

    Header Done

--- src/components/Header/Hero/Hero.jsx
+++ src/components/Header/Hero/Hero.jsx
@@ -2,16 +2,18 @@ const Hero = () => {
   return (
     <div>
       <div className="hero bg-base-200 ">
-        <div className="hero-content flex-row-reverse gap-12">
-          <div className="w-64 my-4 ">
+        <div className="hero-content md:flex-row-reverse  gap-12">
+          <div className="md:w-64 my-4 ">
             <img
               src="https://miro.medium.com/v2/resize:fit:828/format:webp/1*WUIArrwjY-xXIQ-MuS-7YQ.jpeg"
-              className="w-full  rounded-sm shadow-2xl"
+              className="w-full  rounded-sm shadow-2xl "
             />
           </div>
-          <div className="space-y-10">
-            <h1 className="text-5xl font-bold w-10/12 leading-tight">Books to freshen up your bookshelf</h1>
-           
+          <div className=" space-y-4 md:space-y-10">
+            <h1 className="text-4xl md:text-5xl font-bold md:w-10/12 leading-tight">
+              Books to freshen up your bookshelf
+            </h1>
+
             <button className="btn btn-primary text-white ">Get Started</button>
           </div>
         </div>
--- src/components/Header/Navbar/Navbar.jsx
+++ src/components/Header/Navbar/Navbar.jsx
@@ -15,12 +15,14 @@ const Navbar = () => {
       </li>
     </>
   );
+
   return (
-    <div className="md:mx-6 sticky top-0">
-      <div className="navbar border-b">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+    <div className="md:mx-6 sticky top-0 border-b py-2">
+      <div className="navbar">
+        <div className="navbar-start  absolut">
+          {/* Dropdown for mobile */}
+          <div className="dropdown z-50 ">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-5 w-5"
@@ -35,22 +37,25 @@ const Navbar = () => {
                   d="M4 6h16M4 12h8m-8 6h16"
                 />
               </svg>
-            </div>
+            </label>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
             >
               {navLinks}
             </ul>
           </div>
           <a className="btn btn-ghost text-xl font-bold ">
-            <img className="w-6 h-6 mt-1" src={logo} alt="" />
+            <img className="w-6 h-6 mt-1" src={logo} alt="Book Vibe Logo" />
             Book Vibe
           </a>
         </div>
+
+        {/* Navbar for larger screens */}
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">{navLinks}</ul>
         </div>
+        
         <div className="navbar-end gap-3.5">
           <a className="btn">Sign In</a>
           <a className="btn">Sign Up</a>
